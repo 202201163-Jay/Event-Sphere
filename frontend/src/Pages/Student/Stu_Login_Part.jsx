aa2cd59f16f66a1070a42b2920b879f054fcf3f2
Kathan-Kadiya <202201175@daiict.ac.in>
2024-11-23 18:51:58 +0530

    Bug fixes: File Path error fixed!

--- frontend/src/Pages/Student/Stu_Login_Part.jsx
+++ frontend/src/Pages/Student/Stu_Login_Part.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useAuth } from '../../context/AuthProvider';
+import { useAuth } from '../../Context/AuthProvider';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Cookies from "js-cookie";
